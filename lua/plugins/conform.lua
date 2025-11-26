32d174076e99062363e511f99b30f9b109c7353d
k64z <5641897+k64z@users.noreply.github.com>
2025-11-26 20:01:05 +0200

    Add pg_format to Conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,13 @@ return {
     opts = {
         formatters_by_ft = {
             go = { "goimports" },
+            sql = { "pg_format" },
+        },
+        formatters = {
+            pg_format = {
+                command = "pg_format",
+                args = { "-" },
+            },
         },
         format_on_save = {
             timeout_ms = 500,
